295086d1a9f6712813af4a1c243fa9ba0f404cbf
javier <js@silveriofunk.com>
2014-08-14 12:06:23 +0100

    transferred tests to bike_container_spec.rb

--- spec/bike_container_spec.rb
+++ spec/bike_container_spec.rb
@@ -2,6 +2,7 @@ require 'bike_container'
 
 class ContainerHolder; include BikeContainer; end
 
+
 describe BikeContainer do
 
   let(:bike) { Bike.new }
@@ -11,13 +12,46 @@ describe BikeContainer do
     10.times { holder.dock(Bike.new) }
   end
 
-  it "should accept a bike" do        
-    # we expect the holder to have 0 bikes
+  it "should accept a bike" do
+    bike
+    holder
+    #we expect the holder to have 0 bikes
     expect(holder.bike_count).to eq(0)
-    # let's dock a bike into the holder
-    holder.dock(bike)    
-    # now we expect the holder to have 1 bike
+    #let's dock a bike into the holder
+    holder.dock(bike)
+    #we now expect the holder to have 1 bike
     expect(holder.bike_count).to eq(1)
   end
+
+  it "should release a bike" do
+    holder.dock(bike)
+    holder.release(bike)
+    expect(holder.bike_count).to eq(0)
+  end
+
+  it "should know when it's full" do
+    expect(holder).not_to be_full
+    fill_holder holder 
+    expect(holder).to be_full
+  end
+
+  it "should not accept a bike if it is full" do
+    fill_holder holder
+    expect(lambda { holder.dock(bike) }).to raise_error(RuntimeError)
+  end
+
 end
+
+require 'docking_station'
+
+describe DockingStation do
+
+  let(:station) { DockingStation.new(:capacity => 123) }
+
+  it "should allow setting default capacity on initialising" do
+    expect(station.capacity).to eq(123)
+  end
+
+end
+
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,37 +3,4 @@ require "docking_station"
 describe DockingStation do
 	let(:bike) { Bike.new }
 	let(:station) { DockingStation.new(:capacity => 20) }
-
-	def fill_station(station)
-		20.times { station.dock(Bike.new) }
-	end
-
-	it "should accept a bike" do
-		bike
-		station
-		#we expect the station to have 0 bikes
-		expect(station.bike_count).to eq(0)
-		#let's dock a bike into the station
-		station.dock(bike)
-		#we now expect the station to have 1 bike
-		expect(station.bike_count).to eq(1)
-	end
-
-	it "should release a bike" do
-		station.dock(bike)
-		station.release(bike)
-		expect(station.bike_count).to eq(0)
-	end
-
-	it "should know when it's full" do
-		expect(station).not_to be_full
-		fill_station station 
-		expect(station).to be_full
-	end
-
-	it "should not accept a bike if it is full" do
-		fill_station station
-		expect(lambda { station.dock(bike) }).to raise_error(RuntimeError)
-	end
-
 end
