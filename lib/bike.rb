6f4d16daabd88a34db26d45913492584fb038b25
silver-io <javier@silver.io>
2014-10-30 16:38:16 +0000

    remove comments

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,21 +1,15 @@
 class Bike
-	# the initialize method is always called when you create a new
-	# class by typing Bike.new
+
 	def initialize
-	  # all instance variables begin with "@"
-	  # this must be an instance variable because we'll need it
-	  # in other methods
 		fix!()
 	end
 
 	def broken?
-	  # instance variables are accessible in all methods
 	    @broken
 	end
 
 	def break!
 		@broken = true
-	  # and any instance method can update them	
 	end
 
 	def fix!
